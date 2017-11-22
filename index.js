30343e482b81c64c5a4fc304ca324ce35ac61eb4
mikanebu <meiran1991@gmail.com>
2017-11-22 14:12:14 +0600

    [index][s]: added packages into index.js and updated package.json

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-const {DataHub} = require('./lib/utils/datahub.js')
-const {info} = require('./lib/info')
+const cat = require('./lib/cat')
+const {generateDescriptor} = require('./lib/descriptor')
 const {get} = require('./lib/get')
-const {init} = require('./lib/init')
+const info = require('./lib/info')
+const init = require('./lib/init')
 const {login} = require('./lib/login')
 const {authenticate} = require('./lib/login')
-const {writers} = require('./lib/cat')
-const {validate} = require('./lib/validate')
-const {customMarked} = require('./lib/utils/tools.js')
-const {wait} = require('./lib/utils/output/wait')
-const {handleError} = require('./lib/utils/error')
+const validate = require('./lib/validate')
+
+const wait = require('./lib/utils/output/wait')
+const infoOutput = require('./lib/utils/output/info.js')
 const {error} = require('./lib/utils/error')
-const {config} = require('./lib/utils/config')
-const {infoOutput} = require('./lib/utils/output/info.js')
+
+const {Agent} = require('./lib/utils/agent')
+const {checkDpIsThere} = require('./lib/utils/common')
+const config = require('./lib/utils/config')
+const {DataHub} = require('./lib/utils/datahub')
+const {processExcelSheets} = require('./lib/utils/datahub')
+const {handleOutputs} = require('./lib/utils/datahub')
+const {handleError} = require('./lib/utils/error')
+const {customMarked} = require('./lib/utils/tools.js')
+
+
+
 
 // Module API
 
 module.exports = {
-  DataHub,
-  info,
+  cat,
+  generateDescriptor,
   get,
+  info,
   init,
   login,
   authenticate,
-  writers,
   validate,
-  customMarked,
   wait,
-  handleError,
+  infoOutput,
   error,
+  Agent,
+  checkDpIsThere,
   config,
-  infoOutput
+  DataHub,
+  processExcelSheets,
+  handleOutputs,
+  handleError,
+  customMarked
 }
