2f61fb7f975f7bdca461feeab44bb5135206f35a
Rufus Pollock <rufus.pollock@okfn.org>
2017-12-26 23:13:53 +0000

    [info][s]: use description if no readme - fixes #5.
    Issue: data info command should use description if exists and there is no readme (as is common with many standard data packages).
    
    We now check for description in descriptor if readme is empty or null.

--- test/info.test.js
+++ test/info.test.js
@@ -9,8 +9,20 @@ test('infoPackage works', async t => {
   t.true(out.includes('CO2 PPM - Trends in Atmospheric Carbon Dioxide.'))
 })
 
+test('infoPackage works with description and no readme', async t => {
+  const description = 'Aaa\nbbb\n\nccc'
+  const dataset = await data.Dataset.load({
+    title: 'My dataset',
+    description: description,
+    resources: []
+  })
+  const out = info.infoPackage(dataset)
+  t.true(out.includes(description))
+})
+
 test('infoResource works', async t => {
   const resource = data.File.load('test/fixtures/sample.csv')
   const out = await info.infoResource(resource)
   t.true(out.includes('number'))
 })
+
